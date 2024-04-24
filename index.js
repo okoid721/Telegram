0ea760da3ee54223a0b068e706ffe86fb96da3a5
okoid721 <okoid721@gmail.com>
2024-04-24 23:02:51 +0100

    bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -6,9 +7,14 @@ const app = express();
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  const messageText = msg.text;
 
-  bot.sendMessage(chatId, 'Hello, this is a message from your Telegram bot.');
+  if (messageText === '/start') {
+    bot.sendMessage(chatId, 'Welcome to the bot!');
+  }
 });
+
+const PORT = process.env.PORT || 8000;
 
 app.listen({ port: process.env.PORT }, () =>
   console.log(`Server ready at http://localhost:${process.env.PORT}`)
